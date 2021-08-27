4cd8ee2836aa53dfc01e03388b81565bc56662cc
Benjamin Rosario <benjaminfl119@gmail.com>
2021-08-27 16:40:09 -0400

    confihuracion de flask admin y flask migrate

--- config/development.py
+++ config/development.py
@@ -5,8 +5,6 @@ ENV = os.getenv('FLASK_ENV')
 SECRET_KEY = os.urandom(24)
 JWT_SECRET_KEY = os.urandom(24)
 
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE')
-
 SESSION_COOKIE_DOMAIN = 'localhost'
 
 TESTING = True
